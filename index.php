6937dc93f5695a912d315925cbf96af692c18f46
peter muraya <peterkmuraya@gmail.com>
2019-05-21 16:02:47 +0300

    After modifying the invoice/classes.php to include the electricity.php that caused Kenya Power data not to load

--- index.php
+++ index.php
@@ -8,9 +8,15 @@
 //the following shared library. Note how it is placed relative to this project
 require_once "../library/invoice/classes.php";
 
-//The following file contains classes that support the mutall_rental business
+//The following files contains classes that support the mutall_rental business
 //and are specific to it.
+//
+//Classes revolving around the Invoicin system
 require_once "model/invoice/classes.php";
+
+//Classes revolving around Kenya Power Billig.
+require_once "model/electricity.php";
+
 //
 //Now call the index process from the invoice namespace. Pass the home 
 //directory to the page
