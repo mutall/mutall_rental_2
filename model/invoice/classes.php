d0ad9aea4dae5f2284491048b8cfec364695734a
USER <USER@DESKTOP-4IGC7EE>
2019-12-16 11:21:51 +0300

    Wicky's contact change

--- model/invoice/classes.php
+++ model/invoice/classes.php
@@ -145,8 +145,8 @@ class layout_mutall extends layout_label {
             . "Mutall Investment Co. Ltd. <br/>"
             . "P.O. Box 374<br/>"
             . "Kiserian - 00206 <br/>"
-            . "email: mutallcompany@gmail.com<br/>"
-            . "contact:Camilus on 0717 312 320<br/><br/>"
+            . "Email: mutallcompany@gmail.com<br/>"
+            . "Contact:Camilus on 0717 312 320<br/><br/>"
             .date("jS M Y"). "<br/>"
         ."</section>";
         //
